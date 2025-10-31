7b341f76c52586a9820a9b4f3e165477bf3af5c1
maathbluee <mathys.cunsolo@gmail.com>
2025-10-31 09:58:38 +0100

    fix: fixed compatibility, menu  and route errors for iOS

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,62 +1,72 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
-import { useColorScheme } from 'react-native';
-import { Colors } from '@/constants/theme';
-import { IconSymbol } from '@/components/ui/icon-symbol';
-import { HapticTab } from '@/components/haptic-tab';
+import { Tabs } from "expo-router";
+import React from "react";
+import { useColorScheme } from "react-native";
+import { Colors } from "@/constants/theme";
+import { IconSymbol } from "@/components/ui/icon-symbol";
+import { HapticTab } from "@/components/haptic-tab";
 
 export default function TabsLayout() {
   const colorScheme = useColorScheme();
 
   return (
     <Tabs
+      initialRouteName="search"
       screenOptions={{
         headerShown: false,
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
         tabBarButton: HapticTab,
       }}
     >
       <Tabs.Screen
         name="search"
         options={{
-          title: 'Recherche',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="magnifyingglass" color={color} />,
+          title: "Recherche",
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={28} name="magnifyingglass" color={color} />
+          ),
         }}
       />
-      
+
       <Tabs.Screen
-        name="scan"
+        name="scan/scan"
         options={{
-          title: 'Scan',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="qrcode.viewfinder" color={color} />,
+          title: "Scan",
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={28} name="qrcode.viewfinder" color={color} />
+          ),
         }}
       />
 
       <Tabs.Screen
         name="recommendation"
         options={{
-          title: 'Recommandation',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="star.fill" color={color} />,
+          title: "Recommandation",
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={28} name="star.fill" color={color} />
+          ),
         }}
       />
-      
+
       <Tabs.Screen
         name="history"
         options={{
-          title: 'Historique',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="clock.fill" color={color} />,
+          title: "Historique",
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={28} name="clock.fill" color={color} />
+          ),
         }}
       />
 
       <Tabs.Screen
-        name="user/profil"
+        name="user/profile"
         options={{
-          title: 'Profil',
-          tabBarLabel: 'Profil',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="person.fill" color={color} />,
+          title: "Profil",
+          tabBarLabel: "Profil",
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={28} name="person.fill" color={color} />
+          ),
         }}
       />
-
     </Tabs>
   );
 }
