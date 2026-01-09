f0d988099ff25ce73f2db469797e9595a5684d48
Anastasiia A. <95031679+anastasiiaal@users.noreply.github.com>
2026-01-09 15:44:50 +0100

    feat: replace text placeholders by real heading components

--- app/(tabs)/history.tsx
+++ app/(tabs)/history.tsx
@@ -1,10 +1,12 @@
 import { View } from 'react-native';
 import { Text } from "@/components/ui/Text";
+import { Heading } from '@/components/ui/Heading';
 
 export default function History() {
   return (
     <View style={{ flex:1, justifyContent:'center', alignItems:'center' }}>
-      <Text>Historique</Text>
+      <Heading as="h1">Historique</Heading>
+      <Text style={{ marginTop: 16 }}>Ici les elements</Text>
     </View>
   );
 }
--- app/(tabs)/recommendation.tsx
+++ app/(tabs)/recommendation.tsx
@@ -1,10 +1,12 @@
 import { View } from 'react-native';
 import { Text } from "@/components/ui/Text";
+import { Heading } from '@/components/ui/Heading';
 
 export default function Recommandation() {
   return (
     <View style={{ flex:1, justifyContent:'center', alignItems:'center' }}>
-      <Text>Favoris</Text>
+      <Heading as="h1">Favoris</Heading>
+      <Text style={{ marginTop: 16 }}>Texte</Text>
     </View>
   );
 }
