0dd361611d4a905c525f5c8a006b0e29500c7af7
Anastasiia A. <95031679+anastasiiaal@users.noreply.github.com>
2026-01-09 15:33:43 +0100

    feat: use new button component

--- app/(tabs)/user/profile.tsx
+++ app/(tabs)/user/profile.tsx
@@ -9,6 +9,9 @@ import { Text } from "@/components/ui/Text";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useAuth } from "@/lib/auth/AuthContext";
 import { useRouter } from "expo-router";
+import { Button } from "@/components/ui/Button";
+import UserIcon from "@/assets/icons/user.svg";
+import { Colors } from "@/constants/theme";
 
 export default function Profil() {
   const { user, isLoading, logout } = useAuth();
@@ -99,9 +102,15 @@ export default function Profil() {
           )}
         </View>
 
-        <TouchableOpacity style={styles.logoutButton} onPress={handleLogout}>
-          <Text style={styles.logoutButtonText}>Se déconnecter</Text>
-        </TouchableOpacity>
+        <Button
+          label="Se déconnecter"
+          variant="secondary"
+          fullWidth
+          icon={<UserIcon width={20} height={20} fill={Colors.light.primary.base} />}
+          iconPosition="left"
+          containerStyle={{ margin: 20 }} // ✅ mêmes marges que l'ancien bouton
+          onPress={handleLogout}
+        />
       </ScrollView>
     </SafeAreaView>
   );
