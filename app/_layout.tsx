d447fea323219a5d14b7d9d6a44b33b55a6a6e0b
Anastasiia A. <95031679+anastasiiaal@users.noreply.github.com>
2026-01-30 14:18:17 +0100

    fix: status bar style (dark)

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,6 @@
 import { Slot, Redirect } from "expo-router";
 import { AuthProvider, useAuth } from "@/lib/auth/AuthContext";
-import { View, ActivityIndicator, Platform } from "react-native";
+import { View, ActivityIndicator, Platform, StatusBar } from "react-native";
 import { useFonts } from "expo-font";
 import * as SplashScreen from "expo-splash-screen";
 import * as NavigationBar from "expo-navigation-bar";
@@ -37,6 +37,7 @@ export default function RootLayout() {
 
   useEffect(() => {
     if (Platform.OS === "android") {
+      StatusBar.setBarStyle("dark-content");
       NavigationBar.setButtonStyleAsync("dark");
       SystemUI.setBackgroundColorAsync("#FAFAFAFF");
     }
